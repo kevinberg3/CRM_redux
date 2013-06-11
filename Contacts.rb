6a14724f98c0b63a246a20620f1192da5c500881
kevinberg3 <kevinberg3@gmail.com>
2013-06-11 16:09:14 -0400

    second commit

--- Contacts.rb
+++ Contacts.rb
@@ -1,44 +1,70 @@
 class Contacts
 
-    attr_accessor :id, :firstname, :lastname, :email, :notes
+		attr_accessor :id, :firstname, :lastname, :email, :notes
 
-    def initialize(id, firstname, lastname, email, notes)
-        @id = id
-        @firstname = firstname
-        @lastname = lastname
-        @email = email
-        @notes = notes
-    end
+		def initialize(id, firstname, lastname, email, notes)
+				@id = id
+				@firstname = firstname
+				@lastname = lastname
+				@email = email
+				@notes = notes
+		end
 
-    # hash = {
-    #     :id => @id,
-    #     :firstname => @firstname,
-    #     :lastname => @lastname,
-    #     :email => @email,
-    #     :notes => @notes,
-    # }
+		# hash = {
+		#     :id => @id,
+		#     :firstname => @firstname,
+		#     :lastname => @lastname,
+		#     :email => @email,
+		#     :notes => @notes,
+		# }
 
-    def add_contact_to_array(db)
-        db.contacts_array.push(self)
-        # .contacts_array.push(c)
-    end
+		def add_contact_to_array(db)
+				db.contacts_array.push(self)
+				# .contacts_array.push(c)
+		end
+
+		# def to_s
+		#     puts self.firstname
+		#     puts self.lastname
+		#     puts self.email
+		#     puts self.notes
+		#     return "contact_printoff"
+		# end
+# Printing method; not used for testing purposes
 end
 
 
 
 class Database
 
-    attr_accessor :contacts_array
+		attr_accessor :contacts_array
 
-    def initialize
-        @contacts_array = []
-    end
+		def initialize
+				@contacts_array = []
+		end
 
-#     def add_contact(contact)
-#         @contacts_array << contact
-#     end
+		def find_contact(find_attribute, find_me)
 
-# end
+				if find_attribute == "last name"
+				@contacts_array.each { |contact|
+					if contact.lastname == find_me
+						return contact
+					else
+						puts "No matches"
+					end
+				}
+				else
+				  puts "No such input identified."
+				end
+	  end
+
+	  def modify_contact(attribute_to_mod, old_value, new_value)
+	  		find = find_contact(attribute_to_mod, old_value)
+	  		if attribute_to_mod == "last name"
+	  		find.lastname  = new_value
+	  		end
+	  end
+
 
 
 # def compare_first_name(firstname)
@@ -52,3 +78,4 @@ class Database
 #     end
 
 end
+
--- crm_database_test.rb
+++ crm_database_test.rb
@@ -6,19 +6,32 @@ class DatabaseTest < Test::Unit::TestCase
 
     def setup
         @database = Database.new
+        @contact1 = Contacts.new(100, "Kevin", "Berg", "kb@kevin.com", "Needs to improve Ruby skills")
+        @contact2 = Contacts.new(101, "Dylan", "Tredger", "dt@dylan.com", "has a coffee")
+        @contact1.add_contact_to_array(@database)
+        @contact2.add_contact_to_array(@database)
     end
 
     def test_enter_contact
-        contact1 = Contacts.new(100, "Kevin", "Berg", "kb@kevin.com", "Needs to improve Ruby skills")
-        assert_equal "Kevin", contact1.firstname
+        assert_equal "Kevin", @contact1.firstname
     end
 
     def test_display_all_contacts
-        contact1 = Contacts.new(100, "Kevin", "Berg", "kb@kevin.com", "Needs to improve Ruby skills")
-        contact2 = Contacts.new(101, "Dylan", "Tredger", "dt@dylan.com", "has a coffee")
-        contact1.add_contact_to_array(@database)
-        contact2.add_contact_to_array(@database)
+
         assert_equal 2, @database.contacts_array.length
         assert_equal "Dylan", @database.contacts_array[1].firstname
+        # assert_equal "contact_printoff",  @database.puts(contacts_array)
     end
+
+    def test_find_specific_contact
+        assert_equal @contact1, @database.find_contact("last name", "Berg")
+    end
+
+    def test_modify_contact
+        assert_equal "James", @database.modify_contact("last name", "Berg", "James")
+    end
+
 end
+
+
+
